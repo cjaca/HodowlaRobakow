b0fa029be0d89dcc54fd72dc3b3ef917e4cf2b40
cjaca <sownik96@gmail.com>
2019-01-03 02:05:41 +0100

    cleared code

--- HodowlaRobakow/Kid.h
+++ HodowlaRobakow/Kid.h
@@ -39,7 +39,6 @@ public:
 	void gatherInfo(sf::Vector2f position);
 	int getSize();
 	void setSize();
-	void evolution();
 	void hpBar();
 	void setPosition(sf::Vector2f position);
 	bool flagaKolizja = true; //ustawia flage czy ma kolizjowac
